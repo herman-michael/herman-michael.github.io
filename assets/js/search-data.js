8d30dc6fda0195abe901f3ddac20791a20870cc9
herman-michael <michaelallenherman@gmail.com>
2025-03-18 16:48:23 +0000

    Deploying to gh-pages from @ herman-michael/herman-michael.github.io@cd0d9824a3f5c6b66838d889562bb7120dd1e01e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,27 +37,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
-          },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
-          },
-        },{id: "nav-submenus",
-          title: "submenus",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/_pages/dropdown.html";
-          },
         },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
